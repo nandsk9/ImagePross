0edded685afd3c37d5641d3ddaf3d4d6f6c61ae0
nandu <nandsk9@gmail.com>
2021-04-16 20:52:44 +0530

    Initial commit

---
+++ app/src/main/java/com/bot/imagepro/MainActivity.java
@@ -0,0 +1,41 @@
+package com.bot.imagepro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        if(OpenCVLoader.initDebug()){
+            Log.d("mainActivity","opencv is loaded");
+        }
+        else {
+            Log.d("mainActivity","opencv is fail to load");
+        }
+    }
+
+    private Button camera_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        camera_button = findViewById(R.id.camera_btn);
+        camera_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,CameraActivity2.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+    }
+}
+
+//https://www.youtube.com/watch?v=3NVsN72gWJQ&list=PL0aoTDj9Nwgh0hTC3QBHwKtJuxl1veGyG&index=2
