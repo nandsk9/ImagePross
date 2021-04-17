70d84b4113f3c8e8feda08a2904b13a5f872ff77
prajulUdmax <prajulkannikulangara@gmail.com>
2021-04-17 16:36:56 +0530

    part 2 18.53 sec vare type akki

--- app/src/main/java/com/bot/imagepro/CameraActivity.java
+++ app/src/main/java/com/bot/imagepro/CameraActivity.java
@@ -1,14 +1,69 @@
 package com.bot.imagepro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.Loader;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class CameraActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
+public class CameraActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG= "MainActivity";
+    private Mat mRgba;
+    private Mat mGrey;
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status){
+                case LoaderCallbackInterface
+                        .SUCCESS:{
+                    Log.i(TAG,"Opencv is loaded");
+                    mOpenCvCameraView.enableView();
+                }
+                default:
+                {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+    public CameraActivity(){
+        Log.i(TAG,"Instantiated new "+this.getClass());
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_camera);
+        mOpenCvCameraView = (CameraBridgeViewBase)findViewById(R.id.frame_Surface);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG,"Opencv initialize is done");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }else {
+            Log.d(TAG,"Opencv is not loaded try again");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0,this.mLoaderCallback);
+        }
     }
 }
