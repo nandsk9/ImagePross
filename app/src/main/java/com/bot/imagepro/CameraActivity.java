5e056bded76ab312fc0510557744767529cc2c1a
Nanda Kishor vc <nandsk9@users.noreply.github.com>
2021-04-19 11:01:31 +0530

    CameraActivity.java

--- app/src/main/java/com/bot/imagepro/CameraActivity.java
+++ app/src/main/java/com/bot/imagepro/CameraActivity.java
@@ -1,9 +1,13 @@
 package com.bot.imagepro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.loader.content.Loader;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
@@ -14,7 +18,10 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 public class CameraActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -48,6 +55,16 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        int MY_PERMISSIONS_REQUEST_CAMERA =0;
+
+        if (ContextCompat.checkSelfPermission( CameraActivity. this, Manifest.permission.CAMERA) ==PackageManager.PERMISSION_DENIED) {
+
+            ActivityCompat.requestPermissions(CameraActivity.this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+
+        }
+
+
         setContentView(R.layout.activity_camera);
         mOpenCvCameraView = (CameraBridgeViewBase)findViewById(R.id.frame_Surface);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
@@ -63,7 +80,40 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }else {
             Log.d(TAG,"Opencv is not loaded try again");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0,this.mLoaderCallback);
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0,this,mLoaderCallback);
         }
     }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mOpenCvCameraView !=null){
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+    public void onDestroy(){
+        super.onDestroy();
+        if(mOpenCvCameraView !=null){
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+    public void onCameraViewStarted(int width ,int height){
+        mRgba=new Mat(height,width, CvType.CV_8UC4);
+        mGrey =new Mat(height,width,CvType.CV_8UC1);
+    }
+    public void onCameraViewStopped(){
+        mRgba.release();
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame){
+        mRgba=inputFrame.rgba();
+        mGrey =inputFrame.gray();
+
+        //mRgba=CascadeRec(mRgba);
+        return mRgba;
+    }
+
+
 }
+
