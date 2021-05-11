e39279595cd7222b673dbdbfac4e5625d7292246
prajulUdmax <prajulkannikulangara@gmail.com>
2021-05-11 18:27:50 +0530

    fragment added

--- app/src/main/java/com/bot/imagepro/MainActivity.java
+++ app/src/main/java/com/bot/imagepro/MainActivity.java
@@ -3,6 +3,7 @@ package com.bot.imagepro;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.Manifest;
 import android.content.Intent;
@@ -16,14 +17,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView mImageView;
-    Button mUploadBtn;
-    private static final int IMAGE_PICK_CODE=1000;
-    private static final int PERMISSION_CODE=1001;
+    //ImageView mImageView;
+    //Button mUploadBtn;
+   // private static final int IMAGE_PICK_CODE=1000;
+    //private static final int PERMISSION_CODE=1001;
 
 
 
@@ -37,57 +41,86 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private Button camera_button;
+    //private Button camera_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        camera_button = findViewById(R.id.camera_btn);
-        //loading .. dioalog calling
-        LoadingDialog loadingDialog = new LoadingDialog(MainActivity.this);
-        camera_button.setOnClickListener(new View.OnClickListener() {
+        // setting fragment
+        TabLayout tabLayout=findViewById(R.id.tabBar);
+        TabItem tabRealtime =findViewById(R.id.tabRealtime);
+        TabItem tabUpload = findViewById(R.id.tabUpload);
+        ViewPager viewPager=findViewById(R.id.viewPager);
+        PagerAdapter pagerAdapter= new PagerAdapter(getSupportFragmentManager(),
+                tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        //one last step is to change the tabs view when the tab is selected or clicked
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, CameraActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                //loading ....dialog on scan face button
-                loadingDialog.startLoadingDialog();
-                Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        loadingDialog.dismissDialog();
-                    }
-                },1000);
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
+
+
+       // camera_button = findViewById(R.id.camera_btn);
+        //loading .. dioalog calling
+        //LoadingDialog loadingDialog = new LoadingDialog(MainActivity.this);
+      //  camera_button.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+          // public void onClick(View view) {
+              //  startActivity(new Intent(MainActivity.this, CameraActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                //loading ....dialog on scan face button
+               // loadingDialog.startLoadingDialog();
+              //  Handler handler = new Handler();
+               // handler.postDelayed(new Runnable() {
+                  // @Override
+                //    public void run() {
+                     //   loadingDialog.dismissDialog();
+                  //  }
+              //  },2000);
+           // }
+        //});
         //upload image view
-        mImageView=findViewById(R.id.imageView1);
-        mUploadBtn=findViewById(R.id.upload_image_btn);
+       // mImageView=findViewById(R.id.imageView1);
+       // mUploadBtn=findViewById(R.id.upload_image_btn);
         //handle button click
-        mUploadBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+       // mUploadBtn.setOnClickListener(new View.OnClickListener() {
+           // @Override
+           // public void onClick(View view) {
                 //Check run time permission
-                if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
-                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
+                //if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                   // if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
                         //permission not granted ,request it.
                         String[] permissions ={Manifest.permission.READ_EXTERNAL_STORAGE};
                         //show popup for runtime permissions
-                        requestPermissions(permissions,PERMISSION_CODE);
+                       // requestPermissions(permissions,PERMISSION_CODE);
 
-                    }
-                    else {
+                   // }
+                   // else {
                         //permission already granted
-                        pickImageFromGallery();
-                    }
+                     //   pickImageFromGallery();
+                    //}
 
-                }
-                else {
+               // }
+                //else {
                     // system os is less then marshmallow
-                    pickImageFromGallery();
-                }
+                  //  pickImageFromGallery();
+               // }
                 //loading.. dialog on upload image
                 //loadingDialog.startLoadingDialog();
                 //Handler handler = new Handler();
@@ -99,54 +132,54 @@ public class MainActivity extends AppCompatActivity {
                 //},1000);
 
 
-            }
+           // }
 
-        });
+       // });
 
 
 
-    }
+   // }
 
-    private void pickImageFromGallery() {
+   // private void pickImageFromGallery() {
         //intent to pick image
-        Intent intent=new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent,IMAGE_PICK_CODE);
+       // Intent intent=new Intent(Intent.ACTION_PICK);
+      //  intent.setType("image/*");
+       // startActivityForResult(intent,IMAGE_PICK_CODE);
 
 
-    }
+   // }
     // handle result of runtime permission
 
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch(requestCode){
-            case PERMISSION_CODE:{
-                if (grantResults.length>0 && grantResults[0]==
-                PackageManager.PERMISSION_GRANTED){
+    //@Override
+    //public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+      //  switch(requestCode){
+         //   case PERMISSION_CODE:{
+           //     if (grantResults.length>0 && grantResults[0]==
+            //    PackageManager.PERMISSION_GRANTED){
                     //PERMISSION was granted
-                    pickImageFromGallery();
-                }
-                else {
+             //       pickImageFromGallery();
+              //  }
+              //  else {
                     //permission denied
-                    Toast.makeText(this, "Permission denied...!", Toast.LENGTH_SHORT).show();
-                }
+                   // Toast.makeText(this, "Permission denied...!", Toast.LENGTH_SHORT).show();
+              //  }
 
-            }
-        }
-    }
+           // }
+       // }
+    //}
     //handle result of run time permission
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    //@Override
+    //protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
 
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+       // super.onActivityResult(requestCode, resultCode, data);
+        //if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
             //set image to image view
-            mImageView.setImageURI(data.getData());
+          //  mImageView.setImageURI(data.getData());
 
-        }
+       // }
     }
 }
 
---
+++ app/src/main/java/com/bot/imagepro/UploadFragment.java
@@ -0,0 +1,170 @@
+package com.bot.imagepro;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link UploadFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class UploadFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private static final int RESULT_OK = 0;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    ImageView mImageView;
+    Button mUploadBtn;
+    private static final int IMAGE_PICK_CODE=1000;
+    private static final int PERMISSION_CODE=1001;
+
+    public UploadFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment UploadFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static UploadFragment newInstance(String param1, String param2) {
+        UploadFragment fragment = new UploadFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View parentView = inflater.inflate(R.layout.fragment_upload, container, false);
+
+        mImageView= parentView.findViewById(R.id.imageView1);
+        mUploadBtn=parentView.findViewById(R.id.upload_image_btn);
+        //handle button click
+        mUploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check run time permission
+                if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_DENIED){
+                        //permission not granted ,request it.
+                        String[] permissions ={Manifest.permission.READ_EXTERNAL_STORAGE};
+                        //show popup for runtime permissions
+                        requestPermissions(permissions,PERMISSION_CODE);
+
+                    }
+                    else {
+                        //permission already granted
+                        pickImageFromGallery();
+                    }
+
+                }
+                else {
+                    // system os is less then marshmallow
+                    pickImageFromGallery();
+                }
+                //loading.. dialog on upload image
+                //loadingDialog.startLoadingDialog();
+                //Handler handler = new Handler();
+                //handler.postDelayed(new Runnable() {
+                //@Override
+                // public void run() {
+                // loadingDialog.dismissDialog();
+                //}
+                //},1000);
+
+
+            }
+
+        });
+
+
+
+        return parentView;
+    }
+
+
+
+
+    private void pickImageFromGallery() {
+        //intent to pick image
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,IMAGE_PICK_CODE);
+
+
+    }
+    // handle result of runtime permission
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case PERMISSION_CODE:{
+                if (grantResults.length>0 && grantResults[0]==
+                        PackageManager.PERMISSION_GRANTED){
+                    //PERMISSION was granted
+                    pickImageFromGallery();
+                }
+                else {
+                    //permission denied
+                    Toast.makeText(this,"Permission denied...!",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }
+    }
+    //handle result of run time permission
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            //set image to image view
+            mImageView.setImageURI(data.getData());
+
+        }
+    }
+}
